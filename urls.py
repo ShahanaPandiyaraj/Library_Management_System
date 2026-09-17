a21d43aa8f7ee9fd28a0c2b71c27d4cff0ed4771
SHAHANA S <shahana.pandiraj@gmail.com>
2026-09-17 22:52:59 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from catalog import views as catalog_views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', catalog_views.index, name='index'),
-    path('', include('catalog.urls')),
-]
